24f23d70a3165b680b28df9f58c132e5ab920a73
taniapil <tania.pilichou@gmail.com>
2015-11-06 15:56:18 +0000

    Ready for Heroku!

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -2,6 +2,9 @@ class HangpersonGame
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
 
   # Get a word from remote "random word" service
 
@@ -10,6 +13,53 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def guess(letter)
+    if letter.to_s.empty? or letter =~ /[^[:alpha:]]/
+      raise ArgumentError.new("Only alphabetic letters are allowed")
+    end
+    
+    if @word.downcase.include? letter.downcase
+      unless @guesses.include? letter.downcase
+        @guesses << letter.downcase
+        return true
+      end
+      return false
+    else
+      unless @wrong_guesses.include? letter.downcase
+        @wrong_guesses << letter.downcase
+        return true
+      end
+      return false
+    end
+  end
+
+  def word_with_guesses
+    guessed = ""
+    @word.each_char do |c| 
+      if @guesses.include? c
+        guessed << c
+      else
+        guessed << '-'
+      end
+    end
+    return guessed
+  end
+  
+  def check_win_or_lose
+    @word.each_char do |c|
+      if @guesses.include? c
+        next
+      elsif @wrong_guesses.length >= 7
+        return :lose
+      else
+        return :play
+      end
+    end
+    return :win
   end
 
   def self.get_random_word
