8111d9ee1eb1fce36203279952e5eebb18a367b0
taniapil <tania.pilichou@gmail.com>
2015-11-06 16:47:48 +0000

    Deploy to Heroku

--- app.rb
+++ app.rb
@@ -49,17 +49,17 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
+    @game.check_win_or_lose
     erb :show # You may change/remove this line
   end
   
   get '/win' do
-    ### YOUR CODE HERE ###
+    @game.check_win_or_lose
     erb :win # You may change/remove this line
   end
   
   get '/lose' do
-    ### YOUR CODE HERE ###
+    @game.check_win_or_lose
     erb :lose # You may change/remove this line
   end
   
